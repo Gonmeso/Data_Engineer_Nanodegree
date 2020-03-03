f002c166ab2914a909be9b4c0df7870ac1baac9d
gonmeso <gmellizosoto@gmail.com>
2020-03-03 20:50:11 +0100

    Improve style

--- 01_data_modeling_with_postgres/dev_env/create_tables.py
+++ 01_data_modeling_with_postgres/dev_env/create_tables.py
@@ -19,32 +19,39 @@ def _get_table_name(s):
     m = re.search(r'(IF.*EXISTS) ([a-z]+)', s)
     return m.group(2)
 
+
 def create_database():
     """
     - Creates and connects to the sparkifydb
     - Returns the connection and cursor to sparkifydb
     """
-    
+
     # connect to default database
     logging.info('Connecting to default database')
-    conn = psycopg2.connect("host=db dbname=studentdb user=student password=student")
+    conn = psycopg2.connect(
+        "host=db dbname=studentdb user=student password=student"
+        )
     conn.set_session(autocommit=True)
     cur = conn.cursor()
-    
+
     # create sparkify database with UTF8 encoding
     logging.info('Droping sparkifydb if exists')
     cur.execute("DROP DATABASE IF EXISTS sparkifydb")
     logging.info('Creating sparkifydb')
-    cur.execute("CREATE DATABASE sparkifydb WITH ENCODING 'utf8' TEMPLATE template0")
+    cur.execute(
+        "CREATE DATABASE sparkifydb WITH ENCODING 'utf8' TEMPLATE template0"
+        )
 
     # close connection to default database
-    conn.close()    
-    
+    conn.close()
+
     # connect to sparkify database
     logging.info('Connecting to sparkifydb')
-    conn = psycopg2.connect("host=db dbname=sparkifydb user=student password=student")
+    conn = psycopg2.connect(
+        "host=db dbname=sparkifydb user=student password=student"
+        )
     cur = conn.cursor()
-    
+
     return cur, conn
 
 
@@ -60,7 +67,7 @@ def drop_tables(cur, conn):
 
 def create_tables(cur, conn):
     """
-    Creates each table using the queries in `create_table_queries` list. 
+    Creates each table using the queries in `create_table_queries` list.
     """
     for query in create_table_queries:
         logging.info(f'Creating table {_get_table_name(query)}')
@@ -70,19 +77,19 @@ def create_tables(cur, conn):
 
 def main():
     """
-    - Drops (if exists) and Creates the sparkify database. 
-    
+    - Drops (if exists) and Creates the sparkify database.
+
     - Establishes connection with the sparkify database and gets
-    cursor to it.  
-    
-    - Drops all the tables.  
-    
-    - Creates all tables needed. 
-    
-    - Finally, closes the connection. 
+    cursor to it.
+
+    - Drops all the tables.
+
+    - Creates all tables needed.
+
+    - Finally, closes the connection.
     """
     cur, conn = create_database()
-    
+
     drop_tables(cur, conn)
     create_tables(cur, conn)
 
