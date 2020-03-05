4150048f1aa1d422048ab5a71357c884f62c842e
gonmeso <gmellizosoto@gmail.com>
2020-03-05 10:53:32 +0100

    Modify query constraints and add on conflict for inserts

--- 01_data_modeling_with_postgres/dev_env/sql_queries.py
+++ 01_data_modeling_with_postgres/dev_env/sql_queries.py
@@ -15,8 +15,8 @@ songplay_table_create = (
     start_time timestamp,
     user_id varchar NOT NULL,
     level varchar,
-    song_id varchar NOT NULL,
-    artist_id varchar NOT NULL,
+    song_id varchar,
+    artist_id varchar,
     session_id int NOT NULL,
     location varchar,
     user_agent varchar,
@@ -82,28 +82,34 @@ songplay_table_insert = (
                            level, session_id,
                            location, user_agent,
                            song_id, artist_id)
-    VALUES (%s, %s, %s, %s, %s, %s, %s, %s);
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
     """)
 
 user_table_insert = (
     """
     INSERT INTO users (user_id, first_name, last_name,
                        gender, level)
-    VALUES (%s, %s, %s, %s, %s);
+    VALUES (%s, %s, %s, %s, %s)
+    ON CONFLICT (user_id) 
+    DO NOTHING;
     """)
 
 song_table_insert = (
     """
     INSERT INTO songs (song_id, title, artist_id,
                            year, duration)
-    VALUES (%s, %s, %s, %s, %s);
+    VALUES (%s, %s, %s, %s, %s)
+    ON CONFLICT (song_id) 
+    DO NOTHING;
     """)
 
 artist_table_insert = (
     """
     INSERT INTO artists (artist_id, name, location,
                         latitude, longitude)
-    VALUES (%s, %s, %s, %s, %s);
+    VALUES (%s, %s, %s, %s, %s)
+    ON CONFLICT (artist_id) 
+    DO NOTHING;
     """)
 
 
@@ -111,7 +117,9 @@ time_table_insert = (
     """
     INSERT INTO time (start_time, hour, day,
                       week, month, year, weekday)
-    VALUES (%s, %s, %s, %s, %s, %s, %s);
+    VALUES (%s, %s, %s, %s, %s, %s, %s)
+    ON CONFLICT (start_time) 
+    DO NOTHING;
     """)
 
 # FIND SONGS
