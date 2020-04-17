3c920bc58b696773d63c463419b5c7bbf8235a4d
gonmeso <gmellizosoto@gmail.com>
2020-04-17 22:44:58 +0200

    Fill scripst with utils changes

--- 03_data_warehouse/dev_env/etl.py
+++ 03_data_warehouse/dev_env/etl.py
@@ -1,9 +1,10 @@
-import configparser
 import psycopg2
+from utils import get_cluster_connection_str
 from sql_queries import copy_table_queries, insert_table_queries
 
 
 def load_staging_tables(cur, conn):
+    print('---- Loading stage table ----')
     for query in copy_table_queries:
         cur.execute(query)
         conn.commit()
@@ -11,20 +12,20 @@ def load_staging_tables(cur, conn):
 
 def insert_tables(cur, conn):
     for query in insert_table_queries:
+        print(f'---- Inserting into table ----')
         cur.execute(query)
         conn.commit()
 
 
 def main():
-    config = configparser.ConfigParser()
-    config.read('dwh.cfg')
 
-    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
+    conn = psycopg2.connect(get_cluster_connection_str())
+    print('---- Successfully connected to Redshift Cluster ----')
     cur = conn.cursor()
-    
+
     load_staging_tables(cur, conn)
     insert_tables(cur, conn)
-
+    print('---- Process Finished ----')
     conn.close()
 
 
